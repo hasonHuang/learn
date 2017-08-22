256dcf2663f801f8390f272d921f9c3d4fae5f12
hasonHuang <258831020@qq.com>
2017-08-22 20:00:26 +0800

    anno

--- learn-examples/spring-boot/src/main/java/com/hason/springboot/SpringBootExampleApplication.java
+++ learn-examples/spring-boot/src/main/java/com/hason/springboot/SpringBootExampleApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringBootExampleApplication {
 
+    // 通过自定义的 starter pom 自动配置
     @Autowired
     private HelloService helloService;
 
