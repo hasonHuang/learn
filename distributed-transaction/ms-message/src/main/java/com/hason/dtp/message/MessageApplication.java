70980057cab1d0698b0155ba44866e3469ab77cc
hasonHuang <258831020@qq.com>
2017-12-01 22:32:51 +0800

    doc

--- distributed-transaction/ms-message/src/main/java/com/hason/dtp/message/MessageApplication.java
+++ distributed-transaction/ms-message/src/main/java/com/hason/dtp/message/MessageApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 可靠消息服务程序入口
@@ -15,6 +16,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableScheduling
 public class MessageApplication {
 
     public static void main(String[] args) {
--- distributed-transaction/readme.md
+++ distributed-transaction/readme.md
@@ -1,7 +1,11 @@
 # 分布式事务解决方案
 
-## 启动顺序
+## 可靠消息服务
+
+### 启动顺序
 
 1. 启动 ms-discovery-eureka
 2. 启动 ms-message、ms-account、ms-account-web、ms-account-point
 3. 启动 ms-message-consumer
+
+## 最大努力通知
