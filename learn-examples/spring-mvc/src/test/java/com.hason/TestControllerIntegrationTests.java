9f9c447d2666550a2a01b3cf994bb61de203f283
hasonHuang <258831020@qq.com>
2018-05-15 23:22:10 +0800

    遗忘的更新

--- learn-examples/spring-mvc/src/test/java/com.hason/TestControllerIntegrationTests.java
+++ learn-examples/spring-mvc/src/test/java/com.hason/TestControllerIntegrationTests.java
@@ -1,13 +1,6 @@
 package com.hason;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import com.hason.config.MvcConfig;
 import com.hason.service.DemoService;
 import org.junit.Before;
@@ -22,6 +15,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
